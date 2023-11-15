775c0acfead5a04a7ec9b382f7f7169e8b9c1bf8
Anthony Bosek <anthony.bosek@gmail.com>
2023-11-15 15:11:13 -0700

    final refactor

--- server/app.py
+++ server/app.py
@@ -20,40 +20,41 @@ def messages():
     if request.method == "GET":
         messages = Message.query.order_by("created_at").all()
 
-        return make_response([message.to_dict() for message in messages], 200)
+        return [message.to_dict() for message in messages], 200
 
-    elif request.method == "POST":
-        data = request.get_json()
-        message = Message(body=data["body"], username=data["username"])
+    else:
+        req_data = request.get_json()
+        message = Message(body=req_data["body"], username=req_data["username"])
 
         db.session.add(message)
         db.session.commit()
 
-        return make_response(
-            message.to_dict(),
-            201,
-        )
+        return message.to_dict(), 201
 
 
-@app.route("/messages/<int:id>", methods=["PATCH", "DELETE"])
+@app.route("/messages/<int:id>", methods=["GET", "PATCH", "DELETE"])
 def messages_by_id(id):
-    message = Message.query.filter_by(id=id).first()
+    message = db.session.get(Message, id)
+    # message = Message.query.filter_by(id=id).first()
 
-    if request.method == "PATCH":
-        data = request.get_json()
-        for attr in data:
-            setattr(message, attr, data[attr])
+    if request.method == "GET":
+        return message, 200
+
+    elif request.method == "PATCH":
+        req_data = request.get_json()
+        for attr in req_data:
+            setattr(message, attr, req_data[attr])
 
         db.session.add(message)
         db.session.commit()
 
-        return make_response(message.to_dict(), 200)
+        return message.to_dict(), 200
 
     elif request.method == "DELETE":
         db.session.delete(message)
         db.session.commit()
 
-        return make_response({"deleted": True}, 200)
+        return {"message": f"Message id#{message.id} was deleted."}, 200
 
 
 if __name__ == "__main__":
